af36b8ecb07febc4161891fcd843b5ed57639d2f
Jeffrey Vandenborne <jeffrey@vandenborne.co>
2019-06-12 20:35:13 +0200

    Add cors

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,13 @@
-import 'reflect-metadata';
 import fastify from 'fastify';
+import fastifyCors from 'fastify-cors';
+import 'reflect-metadata';
 import users from './routes/users';
 
 const app = fastify({
   logger: process.env.NODE_ENV !== 'test' || Boolean(process.env.FORCE_LOGGING),
 });
 
+app.register(fastifyCors);
 app.register(users, { prefix: '/users' });
 
 export = app;
